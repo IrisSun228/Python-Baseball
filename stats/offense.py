20bff1b1eb949e30177243c64739814730ce9f0c
Iris Sun <irissun@live.com>
2023-01-30 17:25:35 -0500

    Section 4 Step 3 - Convert Column Type

--- stats/offense.py
+++ stats/offense.py
@@ -6,3 +6,4 @@ plays = games[games['type'] == 'play']
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
 hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'), ['inning', 'event']]
+hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
