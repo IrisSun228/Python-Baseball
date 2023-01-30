80e0a19abaa2a9ad1ca5ad0d55a1be5184230ed8
Iris Sun <irissun@live.com>
2023-01-29 21:36:07 -0500

    Section 2 Step 9 - Mean Line !SECTION 2 COMPLETE!

--- stats/attendance.py
+++ stats/attendance.py
@@ -12,4 +12,6 @@ attendance.plot(dx='year', y='attendance', figsize=(15, 7), kind='bar')
 plt.xlabel('Year')
 plt.ylabel('Attendance')
 
+plt.axhline(y=attendance['attendance'].mean(), label='Mean', linestyle='--', color='green')
+
 plt.show()
