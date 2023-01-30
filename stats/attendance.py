6b9c75534c263548bfce9703d47bdb8e35f24f1f
Iris Sun <irissun@live.com>
2023-01-29 21:22:21 -0500

    Section 2 Step 4 - Select Attendance

--- stats/attendance.py
+++ stats/attendance.py
@@ -2,3 +2,4 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from data import games
 
+attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3']]
