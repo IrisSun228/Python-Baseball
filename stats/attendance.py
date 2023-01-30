ae68cbc49c63131f754701b42f03a4857580c216
Iris Sun <irissun@live.com>
2023-01-29 21:25:23 -0500

    Section 2 Step 5 - Column Labels

--- stats/attendance.py
+++ stats/attendance.py
@@ -3,3 +3,5 @@ import matplotlib.pyplot as plt
 from data import games
 
 attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3']]
+attendance.columns = ['year', 'attendance']
+
